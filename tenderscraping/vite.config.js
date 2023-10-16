e50b188c3237af9d7bb73b705a25c4243fcc90e0
Federico Collasius <fede.collasius@gmail.com>
2023-10-16 00:06:01 -0300

    More vite.config.js

--- tenderscraping/vite.config.js
+++ tenderscraping/vite.config.js
@@ -5,4 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === "production" ? "/TenderScraping/" : "/",
+  optimizeDeps: {
+    // include: ['linked-dep'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
 });
